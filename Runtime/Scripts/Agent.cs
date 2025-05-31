6258918a9ee8afe3b307b9d85e8370a4ed2b5b7a
snarlynarwhal <pjlegendreiv@gmail.com>
2025-05-31 18:43:07 -0400

    Added support for ReplanRequest which exits current execution enumerator and re-performs the entire execution sequence (say a character dies or is moved and we need to re-evaluate everything).

--- Runtime/Scripts/Agent.cs
+++ Runtime/Scripts/Agent.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using UnityEngine;
 
 namespace HHG.UtilityAI.Runtime
 {
@@ -10,6 +11,7 @@ namespace HHG.UtilityAI.Runtime
         private readonly ITaskBuilder<TContext> taskBuilder;
         private readonly ITaskSelector<TContext> taskSelector;
         private readonly Dictionary<Task<TContext>, float> scoredTasks = new Dictionary<Task<TContext>, float>();
+        private readonly Stack<IEnumerator> executionStack = new Stack<IEnumerator>();
 
         public Agent(
             IContextProvider<TContext> contextProvider,
@@ -23,32 +25,84 @@ namespace HHG.UtilityAI.Runtime
 
         public IEnumerator Execute()
         {
-            scoredTasks.Clear();
-
-            var context = contextProvider.GetContext();
-            var tasks = taskBuilder.BuildTasks(context);
-            var validTasks = tasks.Where(t => t.Rules.All(r => r.IsValid(t, context)));
-
-            foreach (var task in validTasks)
+            while (true)
             {
-                float totalScore = 0f;
-                float totalWeight = 0f;
+                scoredTasks.Clear();
 
-                foreach (var consideration in task.Considerations)
+                var context = contextProvider.GetContext();
+                var tasks = taskBuilder.BuildTasks(context);
+                var validTasks = tasks.Where(t => t.Rules.All(r => r.IsValid(t, context)));
+
+                foreach (var task in validTasks)
                 {
-                    float score = consideration.Score(task, context);
-                    totalScore += score * consideration.Weight;
-                    totalWeight += consideration.Weight;
+                    float totalScore = 0f;
+                    float totalWeight = 0f;
+
+                    foreach (var consideration in task.Considerations)
+                    {
+                        float score = consideration.Score(task, context);
+                        totalScore += score * consideration.Weight;
+                        totalWeight += consideration.Weight;
+                    }
+
+                    if (totalWeight <= 0f) continue;
+
+                    float finalScore = totalScore / totalWeight;
+                    scoredTasks[task] = finalScore;
                 }
 
-                if (totalWeight <= 0f) continue;
+                var selected = taskSelector.Select(scoredTasks);
+                
+                if (selected == null) yield break;
+                
+                var execution = Flatten(selected.Execute(context));
 
-                float finalScore = totalScore / totalWeight;
-                scoredTasks[task] = finalScore;
+                while (execution.MoveNext())
+                {
+                    if (execution.Current is ReplanRequest) break;
+
+                    yield return execution.Current;
+                }
+
+                if (execution.Current is not ReplanRequest) break;
             }
-
-            var selected = taskSelector.Select(scoredTasks);
-            yield return selected?.Execute(context);
         }
+
+        // Need to flatten the exection enumerator in order
+        // to catch and handle replan requests properly.
+        public IEnumerator Flatten(IEnumerator enumerator)
+        {
+            executionStack.Clear();
+            executionStack.Push(enumerator);
+
+            while (executionStack.Count > 0)
+            {
+                var currentEnumerator = executionStack.Peek();
+
+                if (!currentEnumerator.MoveNext())
+                {
+                    executionStack.Pop();
+                    continue;
+                }
+
+                var current = currentEnumerator.Current;
+
+                if (current is IEnumerator nestedEnumerator)
+                {
+                    executionStack.Push(nestedEnumerator);
+                }
+                else
+                {
+                    yield return current;
+                }
+            }
+        }
+    }
+
+    public class ReplanRequest
+    {
+        public static readonly ReplanRequest Instance = new ReplanRequest();
+
+        private ReplanRequest() { }
     }
 }
